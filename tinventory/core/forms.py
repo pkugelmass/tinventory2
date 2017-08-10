37e6b265d3415b1c32727678b1d27dc65c81a359
Paul Kugelmass <pkugelmass@gmail.com>
2017-08-10 16:47:51 +0000

    New user form only accepts email addresses that end with ontario.ca.

--- tinventory/core/forms.py
+++ tinventory/core/forms.py
@@ -1,15 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.validators import EmailValidator
 
+# much of this authentication stuff taken from https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='Required.')
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
-        
-# much of this authentication stuff taken from https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        (first, second,) = email.split("@")
+        (domain, exn,) = second.split(".")
+        if domain != "ontario" or exn != 'ca':
+            raise forms.ValidationError("Domain must be 'ontario.ca'")
+        return email 
