001a48561788c384f7db8be8f62098048db594ac
Paul Kugelmass <pkugelmass@gmail.com>
2017-08-16 03:23:18 +0000

    Got resource count badges working on topics list page.

--- topics/models.py
+++ topics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from mptt.models import TreeForeignKey, MPTTModel
 from autoslug import AutoSlugField
 
+
 class Topic(MPTTModel):
     title = models.CharField(max_length=50)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='subtopics', db_index=True)
@@ -13,3 +14,9 @@ class Topic(MPTTModel):
         
     def __str__(self):
         return self.title
+        
+    def resources(self):
+          related_resources = list(self.attachment_set.all()) + list(self.link_set.all())
+          return sorted(related_resources, key=lambda r: r.date_modified, reverse=True)
+          
+    
