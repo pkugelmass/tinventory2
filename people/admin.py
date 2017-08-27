43f0b1727ec0e618f7ece0d973644f65d399814c
Paul Kugelmass <pkugelmass@gmail.com>
2017-08-27 02:35:37 +0000

    Created the Actions model and admin configuration. Did the migration.

--- people/admin.py
+++ people/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Action
 
 # Register your models here.
 
 class ProfileAdmin(admin.ModelAdmin):
     list_view=['user', 'ministry', 'role', 'profile_picture', 'date_created', 'last_visit']
+    
 admin.site.register(Profile, ProfileAdmin)
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display=('user', 'verb', 'target', 'created')
+    list_filter=('created',)
+    search_fields = ('verb',)
+    
+admin.site.register(Action, ActionAdmin)
+    
--- people/models.py
+++ people/models.py
@@ -5,6 +5,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from datetime import datetime
 import os
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# PROFILE ------------
 
 class Profile(models.Model):
 
@@ -32,3 +36,16 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+    
+# ACTIVITY STREAM -------------
+
+class Action(models.Model):
+    user = models.ForeignKey(User, related_name='actions', db_index=True)
+    verb = models.CharField(max_length=255)
+    target_type = models.ForeignKey(ContentType, blank=True, null=True, related_name='target_obj')
+    target_id = models.PositiveIntegerField(null=True, blank=True, db_index=True)
+    target = GenericForeignKey('target_type', 'target_id')
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    
+    class Meta:
+        ordering=('-created',)
