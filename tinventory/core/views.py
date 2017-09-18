b0380b5a23611932e5b3e686d1ca37ee4a7261e6
Paul Kugelmass <pkugelmass@gmail.com>
2017-09-18 14:46:33 +0000

    Added the notification email on signup.

--- tinventory/core/views.py
+++ tinventory/core/views.py
@@ -3,6 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from tinventory.core.forms import SignUpForm
 from stronghold.decorators import public
+from django.core.mail import send_mail
+
+def notify_paul(user):
+    new_user_string = "%s (%s)" % (user.get_full_name(), user.username)
+    send_mail(
+        '[Tinventory] New Signup - %s.' % (new_user_string),
+        'A new user - %s has signed up.' % (new_user_string),
+        'tinventory900@gmail.com',
+        ['paul.kugelmass@ontario.ca'],
+        fail_silently=True,
+        )
 
 @public
 def signup(request):
@@ -14,6 +25,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            notify_paul(user)
             return redirect('index')
     else:
         form = SignUpForm()
